c428ebef752668ef091ffc07213eeae936ab6d93
Kateryna Nanyzniuk <katerynananyzniuk@gmail.com>
2021-11-08 12:00:30 +0200

    feat: add 01 and 02 files solutions

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+
+fs.writeFile( filePath, '', (err) => {
+    if (err) throw err
+})
+
+console.log('Hi! Enter something:');
+
+rl.on('line', line => {
+    line = line.trim();
+
+    fs.appendFile( filePath, line + `\n`, err => {
+        if (err) throw err
+    })
+
+    if (line === 'exit') {
+      console.log('Bye!');
+      process.exit(0);
+    } 
+});
